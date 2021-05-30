8fa290ad62b1424d813ac2869ec1fd2c0e88eb68
alan santos <alanjosedossantos011@gmail.com>
2021-05-30 08:49:34 -0300

    Basic static components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer className="footer mt-auto py-0 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          <small>
+            <br />
+            Todos os direitos reservado.
+          </small>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
