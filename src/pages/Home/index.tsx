1b4fdedf97d4bdec452fb424c9b0332b95c545c9
alan santos <alanjosedossantos011@gmail.com>
2021-05-30 10:05:56 -0300

    Home, nevagation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,26 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div>
-      <h1>pagina home</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container py-3">
+        <div className="jumbotron">
+          <h1 className="display-4">My-Sales</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>Esta aplicação consiste em exibir um dashboard de dados.</p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Acessar DashBoard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
 
